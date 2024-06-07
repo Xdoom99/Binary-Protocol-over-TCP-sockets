cd3794db9b3680051e5ff74ad694ac37e162bcbe
Debanksh Guha <155806436+Xdoom99@users.noreply.github.com>
2024-06-07 10:39:46 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUF_SIZE 1024
+
+void handle_client(int client_socket) {
+    unsigned char buffer[BUF_SIZE];
+    int bytes_read;
+
+    while ((bytes_read = recv(client_socket, buffer, BUF_SIZE, 0)) > 0) {
+        printf("Received %d bytes\n", bytes_read);
+        // Echo the data back to the client
+        send(client_socket, buffer, bytes_read, 0);
+    }
+
+    if (bytes_read < 0) {
+        perror("recv");
+    }
+
+    close(client_socket);
+}
+
+int main() {
+    int server_socket, client_socket;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        perror("bind");
+        close(server_socket);
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_socket, 5) == -1) {
+        perror("listen");
+        close(server_socket);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server listening on port %d\n", PORT);
+
+    while (1) {
+        client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_addr_len);
+        if (client_socket == -1) {
+            perror("accept");
+            continue;
+        }
+
+        printf("Accepted connection from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+        handle_client(client_socket);
+    }
+
+    close(server_socket);
+    return 0;
+}
+
